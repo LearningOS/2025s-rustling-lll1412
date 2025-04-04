c6bdcfb616b6526765ab6e6700e1a4fcc24a11f5
l <920595557@qq.com>
2025-04-04 23:07:33 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,9 +8,6 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_none() {
-        panic!("Haha, I dropped the Nones!")
-    }
 
     let my_arr = &[
         -1, -2, -3,
